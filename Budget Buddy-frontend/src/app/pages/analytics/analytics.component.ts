0c65f652f38578eeccbdaf4adde2bf6c0426fdf5
ArjunJain17 <ajain@argusoft.com>
2023-07-19 16:55:30 +0530

    resolved some bugs in budge regarding progressbar and text formatting and improved UI

--- Budget Buddy-frontend/src/app/pages/analytics/analytics.component.ts
+++ Budget Buddy-frontend/src/app/pages/analytics/analytics.component.ts
@@ -216,7 +216,7 @@ export class AnalyticsComponent implements OnInit, AfterViewInit {
       plugins: {
         legend: {
           labels: {
-            color: '#ffffff',
+            color: '#808080',
           },
         },
       },
--- Budget Buddy-frontend/src/app/pages/budget/budget.component.ts
+++ Budget Buddy-frontend/src/app/pages/budget/budget.component.ts
@@ -1,107 +1,248 @@
-import { HttpErrorResponse } from "@angular/common/http";
-import { Component, OnInit } from "@angular/core";
-import Budget from "src/app/model/Buget";
-import CategorizedBudgets from "src/app/model/CategorizedBudgets";
-import SpendCategories from "src/app/model/SpendCategories";
-import User from "src/app/model/User";
-import { BudgetService } from "src/app/services/budget/budget.service";
-import { SharedDataService } from "src/app/services/sharedData/shared-data.service";
-import { TransactionService } from "src/app/services/transaction/transaction.service";
-
+import { Component, OnInit } from '@angular/core';
+import SpendCategories from 'src/app/model/SpendCategories';
+import { NewtransactionComponent } from 'src/app/pages/newtransaction/newtransaction.component';
+import { BudgetService } from 'src/app/services/budget/budget.service';
+import { Observable } from 'rxjs';
+import { TransactionsComponent } from '../transactions/transactions.component';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { SharedDataService } from 'src/app/services/sharedData/shared-data.service';
+import Budget from 'src/app/model/Buget';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { TransactionService } from 'src/app/services/transaction/transaction.service';
 @Component({
-  selector: 'app-budgets',
+  selector: 'app-budget',
   templateUrl: './budget.component.html',
   styleUrls: ['./budget.component.css'],
 })
 export class BudgetComponent implements OnInit {
-  categoryDivision!: SpendCategories;
-  userDetails!: any;
-  budgets!: SpendCategories;
-  newBudgets!: SpendCategories;
-  categorizedBudgets!: CategorizedBudgets;
+  // foodBudget!: number;
+  budgetCategory: SpendCategories = {
+    food: null,
+    transport: null,
+    utilities: null,
+    shopping: null,
+    entertainment: null,
+    housing: null,
+    other: null,
+  };
+  spendCategory: SpendCategories = {
+    food: null,
+    transport: null,
+    utilities: null,
+    shopping: null,
+    entertainment: null,
+    housing: null,
+    other: null,
+  };
   constructor(
-    private transactionService: TransactionService,
+    private budgetService: BudgetService,
     private sharedDataService: SharedDataService,
-    private budgetService: BudgetService
+    private http: HttpClient,
+    private transactionServices: TransactionService
   ) {}
+  userDetails!: any;
+  userId!: number;
   ngOnInit(): void {
     this.sharedDataService.userDetailsObservable.subscribe((res) => {
       this.userDetails = res;
+      // console.log(this.userDetails);
+      this.userId = this.userDetails[0].id;
     });
-    this.budgets = {
-      food: 0,
-      transport: 0,
-      entertainment: 0,
-      utilities: 0,
-      shopping: 0,
-      housing: 0,
-      other: 0,
-    };
-    this.newBudgets = {
-      food: 0,
-      transport: 0,
-      entertainment: 0,
-      utilities: 0,
-      shopping: 0,
-      housing: 0,
-      other: 0,
-    };
-
-    this.getMonthlyCategories();
-    this.getAllBudgets();
-    // this.categorizeBudgets(this.budgets);
-  }
-
-  getMonthlyCategories() {
-    const userId = this.userDetails[0].id;
-    const today = new Date();
-    const date = new Date(today.getTime() + 5.5 * 60 * 60 * 1000)
-      .toISOString()
-      .slice(0, 10);
-
-    this.transactionService.getMonthlyCategoriesSpending(userId, date).subscribe(
-      (res: SpendCategories) => {
-        this.categoryDivision = res;
-      },
-      (error: HttpErrorResponse) => {
-        console.log(error.message);
-      }
-    );
-  }
-
-  getAllBudgets() {
     this.budgetService.getAllBudget(this.userDetails[0].id).subscribe(
-      (res: SpendCategories) => {
-        this.budgets = res;
-        console.log(res);
+      (cat) => {
+        // this.newBudget = res;
+        this.budgetCategory = cat;
       },
       (error: HttpErrorResponse) => {
-        alert(error.message);
+        // console.log(error);
       }
     );
+    this.getCategories();
   }
+  // userId: any = this.userDetails[0].id;
+  baseUrl: string = 'http://localhost:8080/api/v1/budget';
+  budgetAmount!: number;
+  // // console.log(this.userDetails[0].id);
+  foodBudget!: number;
+  transportBudget!: number;
+  utilitiesBudget!: number;
+  shoppingBudget!: number;
+  entertainmentBudget!: number;
+  housingBudget!: number;
+  otherBudget!: number;
+  updatedFoodBudget!: number;
+  updatedTransportBudget!: number;
+  updatedUtilitiesBudget!: number;
+  updatedShoppingBudget!: number;
+  updatedEntertainmentBudget!: number;
+  updatedHousingBudget!: number;
+  updatedOtherBudget!: number;
+  // newBudget: SpendCategories = {
+  //   food: this.foodBudget,
+  //   transport: this.transportBudget,
+  //   utilities: this.utilitiesBudget,
+  //   shopping: this.shoppingBudget,
+  //   entertainment: this.entertainmentBudget,
+  //   housing: this.housingBudget,
+  //   other: this.otherBudget,
+  // }
+  doToggle(category: String) {
+    switch (category) {
+      case 'Food': {
+        this.isFoodGreater = false;
+        this.isFoodUpdateShowing = !this.isFoodUpdateShowing;
+        break;
+      }
+      case 'Transport': {
+        this.isTransportGreater = false;
+        this.isTransportUpdateShowing = !this.isTransportUpdateShowing;
+        break;
+      }
+      case 'Entertainment': {
+        this.isEntertainmentGreater = false;
+        this.isEntertainmentUpdateShowing = !this.isEntertainmentUpdateShowing;
+        break;
+      }
+      case 'Shopping': {
+        this.isShoppingGreater = false;
+        this.isShoppingUpdateShowing = !this.isShoppingUpdateShowing;
+        break;
+      }
+      case 'Utilities': {
+        this.isUtilitiesGreater = false;
+        this.isUtilitiesUpdateShowing = !this.isUtilitiesUpdateShowing;
+        break;
+      }
+      case 'Housing': {
+        this.isHousingGreater = false;
+        this.isHousingUpdateShowing = !this.isHousingUpdateShowing;
+        break;
+      }
+      case 'Other': {
+        this.isOtherGreater = false;
+        this.isOtherUpdateShowing = !this.isOtherUpdateShowing;
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  }
+  setBudget(category: string, amount: Number, fieldId: string) {
+    if (amount < this.getBudgetAmount(category) || amount == null) {
+      this.addRed(fieldId, category);
 
-  addBudget(category: string, amount: number) {
-    console.log('Inside addBudget');
-    const userId = this.userDetails[0].id;
-    const budget: Budget = {
-      
-      userId: userId,
+      // inputElement.classList.remove("border-red-500");
+
+      return;
+    }
+
+    //  this.doToggle(category);
+    let budget = {
+      userId: this.userId,
       category: category,
       amount: amount,
     };
-
+    // console.log(budget);
     this.budgetService.addNewBudget(budget).subscribe(
       (res) => {
-        console.log(res);
-        this.getAllBudgets();
+        // console.log(res);
+        this.budgetService.getAllBudget(this.userId).subscribe(
+          (cat) => {
+            // console.log(res);
+            // this.newBudget = res;
+            this.budgetCategory = cat;
+          },
+          (error: HttpErrorResponse) => {
+            // console.log(error);
+          }
+        );
       },
       (error: HttpErrorResponse) => {
-        alert(error.message);
+        // console.log(error);
       }
     );
   }
+  getBudgetAmount(category: String) {
+    switch (category) {
+      case 'Food': {
+        return this.spendCategory.food;
+        break;
+      }
+      case 'Transport': {
+        return this.spendCategory.transport;
+        break;
+      }
+      case 'Entertainment': {
+        return this.spendCategory.entertainment;
+        break;
+      }
+      case 'Shopping': {
+        return this.spendCategory.shopping;
+        break;
+      }
+      case 'Utilities': {
+        return this.spendCategory.utilities;
+        break;
+      }
+      case 'Housing': {
+        return this.spendCategory.housing;
+        break;
+      }
+      case 'Other': {
+        return this.spendCategory.other;
+        break;
+      }
+      default: {
+        return '';
+        break;
+      }
+    }
+  }
+  isFoodGreater: boolean = false;
+  isTransportGreater: boolean = false;
+  isEntertainmentGreater: boolean = false;
+  isShoppingGreater: boolean = false;
+  isHousingGreater: boolean = false;
+  isUtilitiesGreater: boolean = false;
+  isOtherGreater: boolean = false;
 
+  updateBudget(category: string, amount: Number, fieldId: string) {
+    // console.log('Clicked');
+    //this.spendCategory.food = null
+    if (amount < this.getBudgetAmount(category) || amount == null) {
+      this.addRed(fieldId, category);
+
+      // inputElement.classList.remove("border-red-500");
+
+      return;
+    }
+
+    this.doToggle(category);
+    let budget = {
+      userId: this.userId,
+      category: category,
+      amount: amount,
+    };
+    this.budgetService.addNewBudget(budget).subscribe(
+      (res) => {
+        // console.log(res);
+        this.budgetService.getAllBudget(this.userId).subscribe(
+          (cat) => {
+            // console.log(cat);
+            // this.newBudget = res;
+            this.budgetCategory = cat;
+          },
+          (error: HttpErrorResponse) => {
+            // console.log(error);
+          }
+        );
+      },
+      (error: HttpErrorResponse) => {
+        // console.log(error);
+      }
+    );
+  }
   isFoodUpdateShowing: boolean = false;
   isEntertainmentUpdateShowing: boolean = false;
   isTransportUpdateShowing: boolean = false;
@@ -109,58 +250,146 @@ export class BudgetComponent implements OnInit {
   isUtilitiesUpdateShowing: boolean = false;
   isHousingUpdateShowing: boolean = false;
   isOtherUpdateShowing: boolean = false;
-
   getCategories() {
     const today = new Date();
     const date = new Date(today.getTime() + 5.5 * 60 * 60 * 1000)
       .toISOString()
       .slice(0, 10);
     this.transactionServices
-      .getMonthlyCategories(this.userId, date)
+      .getMonthlyCategoriesSpending(this.userId, date)
       .subscribe((res) => {
         console.log(res);
         this.spendCategory = res;
       });
   }
-
   updateProgressBar(category: String): any {
     // // console.log(this.spendCategory);
     // // console.log(this.budgetCategory);
-
-
-    
     switch (category) {
       case 'Food': {
-        return (((this.budgetCategory.food - this.spendCategory.food)/this.budgetCategory.food)*100).toFixed(2);
+        if (this.budgetCategory.food == null) {
+          return 0;
+        }
+        return (
+          ((this.budgetCategory.food - this.spendCategory.food) /
+            this.budgetCategory.food) *
+          100
+        ).toFixed(2) ;
         break;
       }
       case 'Transport': {
-        //console.log(this.spendCategory.transport);
+        if (this.budgetCategory.transport == null) {
+          return 0;
+        }
 
-        //console.log((((this.budgetCategory.transport - this.spendCategory.transport)/this.budgetCategory.transport)*100).toFixed(2));
-        return (((this.budgetCategory.transport - this.spendCategory.transport)/this.budgetCategory.transport)*100).toFixed(2);
+        return (
+          ((this.budgetCategory.transport - this.spendCategory.transport) /
+            this.budgetCategory.transport) *
+          100
+        ).toFixed(2);
         break;
       }
       case 'Entertainment': {
-       // console.log(this.spendCategory.entertainment)
-        return (((this.budgetCategory.entertainment - this.spendCategory.entertainment)/this.budgetCategory.entertainment)*100).toFixed(2);
+        if (this.budgetCategory.entertainment == null) {
+          return 0;
+        }
+
+        return (
+          ((this.budgetCategory.entertainment -
+            this.spendCategory.entertainment) /
+            this.budgetCategory.entertainment) *
+          100
+        ).toFixed(2);
         break;
       }
       case 'Shopping': {
-        return (((this.budgetCategory.shopping - this.spendCategory.shopping)/this.budgetCategory.shopping)*100).toFixed(2);
+        if (this.budgetCategory.shopping == null) {
+          return 0;
+        }
+        return (
+          ((this.budgetCategory.shopping - this.spendCategory.shopping) /
+            this.budgetCategory.shopping) *
+          100
+        ).toFixed(2);
         break;
       }
       case 'Utilities': {
-        return (((this.budgetCategory.utilities - this.spendCategory.utilities)/this.budgetCategory.utilities)*100).toFixed(2);
+        if (this.budgetCategory.utilities == null) {
+          return 0;
+        }
+        return (
+          ((this.budgetCategory.utilities - this.spendCategory.utilities) /
+            this.budgetCategory.utilities) *
+          100
+        ).toFixed(2);
         break;
       }
       case 'Housing': {
-        return (((this.budgetCategory.housing - this.spendCategory.housing)/this.budgetCategory.housing)*100).toFixed(2);
+        if (this.budgetCategory.housing == null) {
+          return 0;
+        }
+        return (
+          ((this.budgetCategory.housing - this.spendCategory.housing) /
+            this.budgetCategory.housing) *
+          100
+        ).toFixed(2);
         break;
       }
       case 'Other': {
-       // console.log(this.spendCategory.other);
-        return (((this.budgetCategory.other - this.spendCategory.other)/this.budgetCategory.other)*100).toFixed(2);
+        if (this.budgetCategory.other == null) {
+          return 0;
+        }
+        // console.log(this.spendCategory.other);
+        return (
+          ((this.budgetCategory.other - this.spendCategory.other) /
+            this.budgetCategory.other) *
+          100
+        ).toFixed(2);
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  }
+  getColor(category: String) {
+    let progress: number = this.updateProgressBar(category);
+    if (progress > 66) {
+      return '#FFFF';
+    } else if (progress > 33 && progress < 66) {
+      return '#D1D435';
+    } else {
+      return '#FF0000';
+    }
+  }
+  deleteBudget(category: String) {
+    switch (category) {
+      case 'Food': {
+        this.budgetCategory.food = null;
+        break;
+      }
+      case 'Transport': {
+        this.budgetCategory.transport = null;
+        break;
+      }
+      case 'Entertainment': {
+        this.budgetCategory.entertainment = null;
+        break;
+      }
+      case 'Shopping': {
+        this.budgetCategory.shopping = null;
+        break;
+      }
+      case 'Utilities': {
+        this.budgetCategory.utilities = null;
+        break;
+      }
+      case 'Housing': {
+        this.budgetCategory.housing = null;
+        break;
+      }
+      case 'Other': {
+        this.budgetCategory.other = null;
         break;
       }
       default: {
@@ -169,21 +398,43 @@ export class BudgetComponent implements OnInit {
     }
   }
 
-  getColor(category:String){
-    let progress: number = this.updateProgressBar(category);
-    
-    if(progress>66 ){
-      return "#FFFF";
-    }
-    else if(progress>33 && progress <66){
-      return "#d1d435";
-    }
-    else{
-      return "#FF0000";
-    }
-  }
+  addRed(id: string, category: string) {
+    const inputElement = document.getElementById(id) as HTMLInputElement;
 
+    inputElement.classList.add('shadow-alert');
 
-    return this.categorizedBudgets;
+    switch (category) {
+      case 'Food': {
+        this.isFoodGreater = true;
+        break;
+      }
+      case 'Transport': {
+        this.isTransportGreater = true;
+        break;
+      }
+      case 'Entertainment': {
+        this.isEntertainmentGreater = true;
+        break;
+      }
+      case 'Shopping': {
+        this.isShoppingGreater = true;
+        break;
+      }
+      case 'Utilities': {
+        this.isUtilitiesGreater = true;
+        break;
+      }
+      case 'Housing': {
+        this.isHousingGreater = true;
+        break;
+      }
+      case 'Other': {
+        this.isOtherGreater = true;
+        break;
+      }
+      default: {
+        break;
+      }
+    }
   }
 }
